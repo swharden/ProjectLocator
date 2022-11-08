dd69ea1f83441bc165511c63d8e5c76acf6b183e
Scott W Harden <swharden@gmail.com>
2022-11-08 15:44:36 -0500

    php: add details to projects

--- src/backend-php/index.php
+++ src/backend-php/index.php
@@ -1,5 +1,7 @@
 <?php
 
+/* Returns an array of full paths for all files on disk with the given filename.
+ */
 function getFilePaths($filename){
 	$paths = array();
 	$conn = new COM("ADODB.Connection") or die("Cannot start ADO");
@@ -19,33 +21,30 @@ function getFilePaths($filename){
 	return $paths;
 }
 
-function getGroup($projectFilePath, $groupFilePaths){
+/* Returns the full path to the group text file associated with the given project file path.
+ * If no group file exist, returns an empty string.
+ */
+function getGroupFile($projectFilePath, $groupFilePaths){
 	foreach ($groupFilePaths as $groupFilePath){
 		$groupDir = dirname($groupFilePath);
 		if (strstr($projectFilePath, $groupDir)){
-			return $groupDir;
+			return $groupFilePath;
 		}
 	}
-	return "ungrouped";
+	return "";
 }
 
-function getProjectsByGroup($projectFilePaths, $groupFilePaths){
-	$groupedProjects = [];
-	foreach ($projectFilePaths as $projectFilePath){
-		$group = getGroup($projectFilePath, $groupFilePaths);
-		$group = xPath($group);
-		$project = getProject($projectFilePath);
-		$groupedProjects[$group][] = $project;
-	}
-	return $groupedProjects;
-}
-
-function getProject($projectFilePath){
+/* Parse a text file and return path/title/description
+ * The path is the FOLDER the file lives in.
+ * The first line of the file is the TITLE.
+ * Additional lines are the DESCRIPTION.
+ */
+function getFileContents($projectFilePath){
 	$txt = file_get_contents($projectFilePath);
 	$txt = str_replace("\r", "", $txt);
 	$lines = explode("\n", $txt, 2);
 	$title = trim($lines[0], "\n");
-	$description = trim($lines[1], "\n");
+	$description = trim($lines[1], "\n"); // TODO: support markdown
 	return [
 		"path" => xPath(dirname($projectFilePath)),
 		"title" => $title, 
@@ -53,6 +52,7 @@ function getProject($projectFilePath){
 	];
 }
 
+/* Convert a local server path into an X-drive path */
 function xPath($dPath){
 	$xPath = $dPath;
 	$xPath = str_replace("D:\\X_Drive", "X:", $xPath);
@@ -60,9 +60,34 @@ function xPath($dPath){
 	return $xPath;
 }
 
+/* Return a nested object containing groups of projects. */
+function getProjectsByGroup($projectFilePaths, $groupFilePaths){
+	$groupedProjects = [];
+	foreach ($projectFilePaths as $projectFilePath){
+		$groupFilePath = getGroupFile($projectFilePath, $groupFilePaths);
+		$groupName = "GROUP-" . MD5($groupFilePath);
+		$project = getFileContents($projectFilePath);
+		if (!in_array($groupName, $groupedProjects)){
+			if (file_exists($groupFilePath)){
+				$groupDetails = getFileContents($groupFilePath);
+				$groupedProjects[$groupName]["path"] = $groupDetails["path"];
+				$groupedProjects[$groupName]["title"] = $groupDetails["title"];
+				$groupedProjects[$groupName]["description"] = $groupDetails["description"];
+			} else {
+				$groupedProjects[$groupName]["path"] = "";
+				$groupedProjects[$groupName]["title"] = "ungrouped";
+				$groupedProjects[$groupName]["description"] = "projects with no group file";
+			}
+		}
+		$groupedProjects[$groupName]["projects"][] = $project;
+	}
+	return $groupedProjects;
+}
+
 $projectFilePaths = getFilePaths("project.txt");
 $groupFilePaths = getFilePaths("project-group.txt");
 $groupedProjects = getProjectsByGroup($projectFilePaths, $groupFilePaths);
 
+header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 echo json_encode($groupedProjects, JSON_PRETTY_PRINT);
